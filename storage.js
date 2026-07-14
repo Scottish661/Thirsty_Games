493392535dc44f724166f1877a1008ffaa9ba918
Scottish661 <h0785014@gmail.com>
2026-07-14 18:18:22 +0100

    Implement resetGame function for score reset
    Added resetGame function to reset score.

--- storage.js
+++ storage.js
@@ -1,1 +1,5 @@
 const score = JSON.parse(localStorage.getItem('score')) || { Wins: 0, Losses: 0, Ties: 0};
+function resetGame(){
+score = { Wins: 0, Losses: 0, Ties: 0}
+  localStorage.getItem('score')
+}
