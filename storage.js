ba0986c855043dd116b1c780e807943914893aa5
Scottish661 <h0785014@gmail.com>
2026-07-15 20:20:40 +0100

    Fix alert message format in resetGame function

--- storage.js
+++ storage.js
@@ -1,2 +1,2 @@
 let score = JSON.parse(localStorage.getItem('score')) || { Wins: 0, Losses: 0, Ties: 0 };
-function resetGame(){score = { Wins: 0, Losses: 0, Ties: 0 };  localStorage.setItem('score', JSON.stringify(score)); alert(`${score}`}
+function resetGame(){score = { Wins: 0, Losses: 0, Ties: 0 };  localStorage.setItem('score', JSON.stringify(score)); alert(JSON.stringify(score)}
