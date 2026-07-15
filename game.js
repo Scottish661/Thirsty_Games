4bc71cd98d766854d0f58e5639148fe8b859e42b
Scottish661 <h0785014@gmail.com>
2026-07-15 22:35:57 +0100

    Refactor play function to use text content for results
    Update the play function to display results in a text element instead of an alert.

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ function play(move){
   move === cm
   ? (r = "Tie", score.Ties++) : rules[move].includes(cm)   ? (r = "You Win", score.Wins++)   : (r = "You Lose", score.Losses++)
   localStorage.setItem('score', JSON.stringify(score))
-  alert(
+  h.textContent = 
     `you chose ${move}, computer chose ${cm}, ${r}\n` +
     `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`
   )
