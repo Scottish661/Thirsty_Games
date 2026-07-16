c7207c44a6ecaddd85d35692b53ddbfc8ca767d1
Scottish661 <h0785014@gmail.com>
2026-07-16 01:01:21 +0100

    Refactor UI code for button and header generation

--- ui.js
+++ ui.js
@@ -5,6 +5,5 @@ for(let drink of allDrinks){
     text: "white"  }};
 const buttonArea = document.getElementById("buttons");for (let drink of allDrinks){buttonArea.innerHTML += 
   `<button style="background:${colors[drink].background}; color:${colors[drink].text} margin:5px";
-  onclick="play('${drink}')">${drink}</button>`;
-}
+  onclick="play('${drink}')">${drink}</button>`};
 const h = document.getElementById("h");for (let drink of allDrinks){ h.innerHTML += `<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
