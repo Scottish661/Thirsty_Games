4b515b577008fdb3774e5e762d0c3481ac86e006
Scottish661 <h0785014@gmail.com>
2026-07-17 00:29:57 +0100

    Fix formatting and syntax in play function

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-function play(move){  const cm = drinks[Math.floor(Math.random() * drinks.length)]  let r = "" 
+function play(move){  const cm = drinks[Math.floor(Math.random() * drinks.length)]  
+    let r = "" 
     move === cm ? (r = "Tie", score.Ties++)  : rules[move].includes(cm)  ? (r = "You Win", score.Wins++) : (r = "You Lose", score.Losses++);
   localStorage.setItem('score', JSON.stringify(score))hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +  
     `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`;}
