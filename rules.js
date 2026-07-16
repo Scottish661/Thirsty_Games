c0fdec92ffb773a5beacdbb40a350a0916548809
Scottish661 <h0785014@gmail.com>
2026-07-16 08:39:52 +0100

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,6 +1,6 @@
 const countries = [ 'Scottish', 'English', 'American'];
-const drinks = [ 'Irn Bru', 'Pepsi', 'Seven Up', 'Coke', 'Lemonade', 'Tea', 'Fanta', 'Sprite', 'Water', 'Coffee', 'Milk', 'Juice',
-'Cola Zero', 'Dr Pepper'];
+const drinks = [ 'Irn Bru', 'Pepsi', 'Seven Up', 'Coke', 'Lemonade', 'Tea', 'Fanta', 'Sprite', 'Water', 'Coffee', 'Milk','Cola Zero',
+                'Dr Pepper'];
 const rules = {};
 let allDrinks = [];
 for (let country of countries) { for (let drink of drinks) {   
@@ -9,7 +9,7 @@ for (let country of countries) { for (let drink of drinks) {
     }}
 for (let drink of allDrinks) {
     rules[drink] = [];
-    while (rules[drink].length < 45) {
+    while (rules[drink].length < 19) {
         let randomDrink =   allDrinks[Math.floor(Math.random() * allDrinks.length)];
         if (  randomDrink !== drink && !rules[drink].includes(randomDrink)
         ) {    rules[drink].push(randomDrink);  }}}
