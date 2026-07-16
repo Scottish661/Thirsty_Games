fae96072d33f69bcd4ab0802fb25819f63ec59f4
Scottish661 <h0785014@gmail.com>
2026-07-17 00:28:26 +0100

    Fix syntax errors in rules.js

--- rules.js
+++ rules.js
@@ -1,4 +1,8 @@
-const drinks = [  'Irn Bru', 'Pepsi', 'Seven Up', 'Coke', 'Lemonade',  'Tea',  'Fanta',  'Sprite', 'Coffee', 'Cola Zero',  'Dr Pepper',
-'Milk','Apple_Juice','water','Orange_juice'];const rules = {} for (let drink of drinks) {rules[drink] = [];
-  while (rules[drink].length < 5) { let randomDrink =  drinks[Math.floor(Math.random() * drinks.length)];
-  if ( randomDrink !== drink && !rules[drink].includes(randomDrink) {rules[drink].push(randomDrink); }}}
+const drinks=['Irn Bru','Pepsi','Seven Up','Coke','Lemonade','Tea','Fanta','Sprite','Coffee','Cola Zero','Dr Pepper','Milk','Apple_Juice',
+'water','Orange_juice'];
+const rules={};
+for(let drink of drinks){
+  rules[drink]=[];
+  while(rules[drink].length<5){
+    let randomDrink=drinks[Math.floor(Math.random()*drinks.length)];
+    if(randomDrink!==drink&&!rules[drink].includes(randomDrink)){ rules[drink].push(randomDrink);}  }}
