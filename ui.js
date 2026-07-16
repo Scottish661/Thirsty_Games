89dabce2abd011a4b92fa7c8f9fb891f027a9853
Scottish661 <h0785014@gmail.com>
2026-07-16 01:00:51 +0100

    Refactor drink button and rules display logic

--- ui.js
+++ ui.js
@@ -1,17 +1,10 @@
 const colors = {};
-for(let drink of drinks){
+for(let drink of allDrinks){
   colors[drink] = {
     background: `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`,
-    text: "white"
-  };
+    text: "white"  }};
+const buttonArea = document.getElementById("buttons");for (let drink of allDrinks){buttonArea.innerHTML += 
+  `<button style="background:${colors[drink].background}; color:${colors[drink].text} margin:5px";
+  onclick="play('${drink}')">${drink}</button>`;
 }
-  const buttonArea = document.getElementById("buttons");
-for (let drink of allDrinks){
-buttonArea.innerHTML += 
-`<button style="background:${colors[drink].background};color:${colors[drink].text}; margin:5px" onclick="play('${drink}')">${drink}</button>`
-}
-const h =  document.getElementById("h")
-for (let drink of drinks){
-  h.innerHTML += 
-  `<b> ${drink}</b> → ${rules[drink]}<br>`
-}
+const h = document.getElementById("h");for (let drink of allDrinks){ h.innerHTML += `<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
