3804a41d6d5bf31b924a2fc3e3a5a16bce555038
Scottish661 <h0785014@gmail.com>
2026-07-17 00:19:01 +0100

    Fix resetGame function to update score display

--- storage.js
+++ storage.js
@@ -1,4 +1,3 @@
 let score = JSON.parse(localStorage.getItem('score')) || { Wins: 0, Losses: 0, Ties: 0 };
 function resetGame(){score = { Wins: 0, Losses: 0, Ties: 0 };  localStorage.setItem('score', JSON.stringify(score)); 
-hh.textContent = `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`
-}
+hh.textContent = `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`}
