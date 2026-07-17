eb3e6ee77a841d712570267513a70284bebb1f07
Scottish661 <h0785014@gmail.com>
2026-07-17 01:20:38 +0100

    Adjust rules for dynamic winsNeeded calculation
    Updated the rules to require a dynamic number of wins based on the number of drinks.

--- rules.js
+++ rules.js
@@ -1,8 +1,9 @@
 const drinks=['Irn Bru','Pepsi','Seven Up','Coke','Lemonade','Tea','Fanta','Sprite','Coffee','Cola Zero','Dr Pepper','Milk','Apple_Juice',
 'water','Orange_juice'];
 const rules={};
+const winsNeeded = (drinks.length - 1) / 2;
 for(let drink of drinks){
   rules[drink]=[];
-  while(rules[drink].length<7){
+  while(rules[drink].length<winsNeeded){
     let randomDrink=drinks[Math.floor(Math.random()*drinks.length)];
     if(randomDrink!==drink&&!rules[drink].includes(randomDrink)){ rules[drink].push(randomDrink);}  }}
