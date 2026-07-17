b2b55986c72f0b8f2534bbbcc966b7bda9c97156
Scottish661 <h0785014@gmail.com>
2026-07-17 01:08:09 +0100

    Fix formatting issues in ui.js

--- ui.js
+++ ui.js
@@ -3,4 +3,4 @@ for(let drink of drinks){ colors[drink] = { background: `rgb(${Math.floor(Math.r
 ${Math.floor(Math.random()*256)})`,text: "white"  }}const buttonArea = document.getElementById("buttons");
 for(let drink of drinks){buttonArea.innerHTML+=`<button style="background:${colors[drink].background}color:${colors[drink].text}margin:5px"
   onclick="play('${drink}')">${drink}</button>`}const h = document.getElementById("h");for
-  (let drink of allDrinks){ h.innerHTML += `<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
+  (let drink of drinks){ h.innerHTML += `<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
