81cc26d4e38b4dd788bc316e0d475601f45e8fdb
Scottish661 <h0785014@gmail.com>
2026-07-17 01:16:12 +0100

    Refactor play function for improved readability

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 function play(move){  const cm = drinks[Math.floor(Math.random() * drinks.length)]  
     let r = "" 
-    move === cm ? (r = "Tie", score.Ties++)  : rules[move].includes(cm)  ? (r = "You Win", score.Wins++) : (r = "You Lose", score.Losses++);
+    move === cm ? (r = "Tie", score.Ties++)  : rules[move].includes(cm)  ? (r = "You Win", score.Wins++)
+        : (r = "You Lose", score.Losses++);
   localStorage.setItem('score', JSON.stringify(score))
       hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +  
     `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`;}
