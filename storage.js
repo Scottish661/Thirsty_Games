7e61460cb6a172c018bc550f0fd5028d20c99c0f
Scottish661 <h0785014@gmail.com>
2026-07-17 01:31:34 +0100

    Remove resetGame function from storage.js

--- storage.js
+++ storage.js
@@ -1,3 +1,1 @@
 let score = JSON.parse(localStorage.getItem('score')) || { Wins: 0, Losses: 0, Ties: 0 };
-function resetGame(){score = { Wins: 0, Losses: 0, Ties: 0 };  localStorage.setItem('score', JSON.stringify(score)); 
-hh.textContent = `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`}
