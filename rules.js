94cca945a8985dfb3f42af9ba8e7e884ea1ee4c6
Scottish661 <h0785014@gmail.com>
2026-07-17 22:35:46 +0100

    Refactor rules.js for drink matchups
    Refactor drink matchup logic and simplify rules generation.

--- rules.js
+++ rules.js
@@ -1,20 +1,11 @@
-const drinks = ['Irn Bru', 'Pepsi', 'Seven Up', 'Coke', 'Lemonade', 'Tea', 'Fanta', 'Sprite', 'Coffee', 'Cola Zero', 'Dr Pepper', 'Milk',
-'Apple Juice', 'Water', 'Orange Juice', 'Hot Chocolate', 'Ginger Ale', 'Root Beer', 'Energy Drink', 'Arnold Palmer', 'Shirley Temple',
-'Grape Juice', 'Cranberry Juice', 'Iced Tea', 'Limeade', 'Sparkling Water', 'Chocolate Milk', 'Mango Juice', 'Pineapple Juice'];
-const rules={};
-const winsNeeded = (drinks.length - 1) / 2;
-for(let drink of drinks){
-  rules[drink]=[];
-  while(rules[drink].length<winsNeeded){
-    let randomDrink=drinks[Math.floor(Math.random()*drinks.length)];
-    if(randomDrink!==drink&&!rules[drink].includes(randomDrink)){ rules[drink].push(randomDrink);}  }}
-
-    if (
-      randomDrink !== drink &&
-      !rules[drink].includes(randomDrink) &&
-      !rules[randomDrink].includes(drink)
-    ) {
-      rules[drink].push(randomDrink);
-    }
-  }
-}
+const drinks=['Irn Bru','Pepsi','Seven Up','Coke','Lemonade','Tea','Fanta','Sprite','Coffee','Cola Zero','Dr Pepper','Milk','Apple Juice',
+'Water','Orange Juice','Hot Chocolate','Ginger Ale','Root Beer','Energy Drink','Arnold Palmer','Shirley Temple','Grape Juice',
+'Cranberry Juice','Iced Tea','Limeade','Sparkling Water','Chocolate Milk','Mango Juice','Pineapple Juice'];
+const rules={},winsNeeded=(drinks.length-1)/2;
+for(let drink of drinks)rules[drink]=[];
+let matchups=[];
+for(let i=0;i<drinks.length;i++)for(let j=i+1;j<drinks.length;j++)matchups.push([drinks[i],drinks[j]]);
+for(let i=matchups.length-1;i>0;i--){let randomIndex=Math.floor(Math.random()*(i+1));
+[matchups[i],matchups[randomIndex]]=[matchups[randomIndex],matchups[i]]}
+for(let match of matchups){let drink1=match[0],drink2=match[1];if(rules[drink1].length<winsNeeded&&rules[drink2].length<winsNeeded)
+{if(Math.random()<0.5)rules[drink1].push(drink2);else rules[drink2].push(drink1)}}
