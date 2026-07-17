1d726495a7dd669490f00c81f88269bb77e73768
Scottish661 <h0785014@gmail.com>
2026-07-17 22:42:21 +0100

    Refactor rules generation logic in rules.js

--- rules.js
+++ rules.js
@@ -1,11 +1,13 @@
 const drinks=['Irn Bru','Pepsi','Seven Up','Coke','Lemonade','Tea','Fanta','Sprite','Coffee','Cola Zero','Dr Pepper','Milk','Apple Juice',
 'Water','Orange Juice','Hot Chocolate','Ginger Ale','Root Beer','Energy Drink','Arnold Palmer','Shirley Temple','Grape Juice',
 'Cranberry Juice','Iced Tea','Limeade','Sparkling Water','Chocolate Milk','Mango Juice','Pineapple Juice'];
-const rules={},winsNeeded=(drinks.length-1)/2;
+function makeRules(){
+const rules={},winsNeeded=(drinks.length-1)/2,matchups=[];
 for(let drink of drinks)rules[drink]=[];
-let matchups=[];
 for(let i=0;i<drinks.length;i++)for(let j=i+1;j<drinks.length;j++)matchups.push([drinks[i],drinks[j]]);
-for(let i=matchups.length-1;i>0;i--){let randomIndex=Math.floor(Math.random()*(i+1));
-[matchups[i],matchups[randomIndex]]=[matchups[randomIndex],matchups[i]]}
-for(let match of matchups){let drink1=match[0],drink2=match[1];if(rules[drink1].length<winsNeeded&&rules[drink2].length<winsNeeded)
-{if(Math.random()<0.5)rules[drink1].push(drink2);else rules[drink2].push(drink1)}}
+for(let i=matchups.length-1;i>0;i--){let j=Math.floor(Math.random()*(i+1));[matchups[i],matchups[j]]=[matchups[j],matchups[i]]}
+for(let [a,b] of matchups){if(rules[a].length<winsNeeded&&rules[b].length<winsNeeded){if(Math.random()<0.5)rules[a].push(b);
+else rules[b].push(a)}}
+return drinks.every(d=>rules[d].length===winsNeeded)?rules:makeRules();
+}
+const rules=makeRules();
