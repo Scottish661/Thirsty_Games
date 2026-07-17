972a53d081c8f37a816c61ef6fe7fb4230d92121
Scottish661 <h0785014@gmail.com>
2026-07-17 01:36:11 +0100

    Refactor color assignment and button creation logic

--- ui.js
+++ ui.js
@@ -7,3 +7,11 @@ for(let drink of drinks){buttonArea.innerHTML+=
   margin:5px" onclick="play('${drink}')">${drink}</button>`;}
 const h=document.getElementById("h");
 for(let drink of drinks){h.innerHTML+=`<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
+function resetGame(){score = { Wins: 0, Losses: 0, Ties: 0 };  localStorage.setItem('score', JSON.stringify(score)); 
+hh.textContent = `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`
+    for(let drink of drinks){colors[drink]={background:
+  `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`,text:"white"}};
+const buttonArea=document.getElementById("buttons");
+for(let drink of drinks){buttonArea.innerHTML+=
+  `<button style="background:${colors[drink].background};color:${colors[drink].text};                 
+}
