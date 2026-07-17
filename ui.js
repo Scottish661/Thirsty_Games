9db6a8f68f2bc947042ba7f3dd659982d09bda31
Scottish661 <h0785014@gmail.com>
2026-07-17 01:13:28 +0100

    Refactor color assignment and button creation logic

--- ui.js
+++ ui.js
@@ -1,6 +1,9 @@
-const colors = {};
-for(let drink of drinks){ colors[drink] = { background: `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},
-${Math.floor(Math.random()*256)})`,text: "white"  }}const buttonArea = document.getElementById("buttons");
-for(let drink of drinks){buttonArea.innerHTML+=`<button style="background:${colors[drink].background}color:${colors[drink].text}margin:5px"
-  onclick="play('${drink}')">${drink}</button>`}const h = document.getElementById("h");for
-  (let drink of drinks){ h.innerHTML += `<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
+const colors={};
+for(let drink of drinks){colors[drink]={background:
+  `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`,text:"white"}};
+const buttonArea=document.getElementById("buttons");
+for(let drink of drinks){buttonArea.innerHTML+=
+  `<button style="background:${colors[drink].background};color:${colors[drink].text};
+  margin:5px" onclick="play('${drink}')">${drink}</button>`;}
+const h=document.getElementById("h");
+for(let drink of drinks){h.innerHTML+=`<b>${drink}</b> → ${rules[drink].join(", ")}<br>`;}
