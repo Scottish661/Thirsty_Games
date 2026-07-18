c5e209edf8c1d728be2fbc496a295d257cc1e844
Scottish661 <h0785014@gmail.com>
2026-07-18 12:45:59 +0100

    Refactor drink matchup rules generation logic

--- rules.js
+++ rules.js
@@ -1,12 +1,10 @@
 const drinks=['Irn Bru','Pepsi','Seven Up','Coke','Lemonade','Tea','Fanta','Sprite','Coffee','Cola Zero','Dr Pepper','Milk','Apple Juice',
 'Water','Orange Juice','Hot Chocolate','Ginger Ale','Root Beer','Energy Drink','Arnold Palmer','Shirley Temple','Grape Juice',
 'Cranberry Juice','Iced Tea','Limeade','Sparkling Water','Chocolate Milk','Mango Juice','Pineapple Juice'];
-function makeRules(){
-while(true){
-const rules={},winsNeeded=(drinks.length-1)/2,matchups=[];
-for(let drink of drinks)rules[drink]=[];
-for(let i=0;i<drinks.length;i++)for(let j=i+1;j<drinks.length;j++)matchups.push([drinks[i],drinks[j]]);
-for(let i=matchups.length-1;i>0;i--){let j=Math.floor(Math.random()*(i+1));[matchups[i],matchups[j]]=[matchups[j],matchups[i]]}
-for(let [a,b] of matchups){if(rules[a].length<winsNeeded&&rules[b].length<winsNeeded){if(Math.random()<0.5)
-rules[a].push(b);else rules[b].push(a)}}
-if(drinks.every(d=>rules[d].length===winsNeeded))return rules;}}
+const rules = {};
+for (let drink of drinks) {
+    rules[drink] = [];
+    while (rules[drink].length < (drinks.length - 1)/2) {
+        let randomDrink =   drinks[Math.floor(Math.random() * drinks.length)];
+        if (  randomDrink !== drink && !rules[drink].includes(randomDrink)
+        ) {    rules[drink].push(randomDrink);  }}}
