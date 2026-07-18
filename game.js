c2ebf3f694bd8d3d3089d37b0a1df3124c83e0ad
Scottish661 <h0785014@gmail.com>
2026-07-18 13:20:33 +0100

    Refactor play function for better readability

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 function play(move) {
   const cm = drinks[Math.floor(Math.random() * drinks.length)];
-  let r = ""; move === cm  ? (r = "Tie", score.Ties++) : rules[move].includes(cm) && rules[cm].includes(move)
-? Math.random() < 0.5 ? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++): rules[move].includes(cm)
-? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++);localStorage.setItem("score", JSON.stringify(score));
-  hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +`Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`;}
+  let r = ""; move === cm  ? (r = "Tie", score.Ties++) :
+    rules[move].includes(cm) && rules[cm].includes(move)
+? Math.random() < 0.5 ? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++):
+    rules[move].includes(cm)
+? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++);
+  localStorage.setItem("score", JSON.stringify(score));
+  hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +`Wins: ${score.Wins} | 
+  Losses: ${score.Losses} | Ties: ${score.Ties}`;}
