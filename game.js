e284e0815d3ceba5be71a3e6811df2c92e91f779
Scottish661 <h0785014@gmail.com>
2026-07-18 12:38:53 +0100

    Refactor play function for game logic

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
-function play(move){  const cm = drinks[Math.floor(Math.random() * drinks.length)]  
-    let r = "" 
-    move === cm ? (r = "Tie", score.Ties++)  : rules[move].includes(cm)  ? (r = "You Win", score.Wins++)
-        : (r = "You Lose", score.Losses++);
-  localStorage.setItem('score', JSON.stringify(score))
-      hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +  
-    `Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`;}
+function play(move) {
+  const cm = allDrinks[Math.floor(Math.random() * allDrinks.length)];
+  let r = ""; move === cm  ? (r = "Tie", score.Ties++) : rules[move].includes(cm) && rules[cm].includes(move)
+? Math.random() < 0.5 ? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++): rules[move].includes(cm)
+? (r = "You Win", score.Wins++): (r = "You Lose", score.Losses++);localStorage.setItem("score", JSON.stringify(score));
+  hh.textContent =`you chose ${move}, computer chose ${cm}, ${r}\n` +`Wins: ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`;}
