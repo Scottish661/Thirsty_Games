cc937f25d773a9dec8810b88c0e75332d2adde75
Scottish661 <h0785014@gmail.com>
2026-07-19 12:23:18 +0100

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-function loadGame(){
-const colors={};
-for(let drink of drinks){colors[drink]={
-background:`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`,text:"white" };}
-const buttonArea=document.getElementById("buttons");
-buttonArea.innerHTML="";
-for(let drink of drinks){ buttonArea.innerHTML+=  `<button style="background:${colors[drink].background};
-  color:${colors[drink].text};  margin:5px" onclick="play('${drink}')">${drink}</button>`;
-}}function resetGame(){loadGame();}loadGame();
