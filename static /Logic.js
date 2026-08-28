ecb4d741f30f810bea541a917d8463822c28e295
Scottish661 <h0785014@gmail.com>
2026-08-28 19:56:40 +0100

    Update Logic.js

--- static /Logic.js
+++ static /Logic.js
@@ -1,1 +1,23 @@
-let a = 0
+let button = document.getElementById("button");
+let gameDrinks;
+  let h = document.getElementById("h");
+function ChangeColor() {
+    let input = document.getElementById("input").value;
+    document.body.style.backgroundColor = input;
+    localStorage.setItem("backgroundColor", input);}
+let savedColor = localStorage.getItem("backgroundColor");
+if (savedColor) {
+    document.body.style.backgroundColor = savedColor;
+    document.getElementById("input").value = savedColor;}
+const score = JSON.parse(localStorage.getItem('score')) || {
+  Wins: 0,Losses: 0,Ties: 0}
+const drinks = [
+  'Iron Brew','Blue Pop','Lemon Up','Classic Cola','Lemonade','Tea','Orange Fizz','Lime Fizz',
+  'Water','Coffee','Milk','Juice','Zero Cola','Doc Fizz','Ginger Ale',
+  'Tonic','Energy Drink','Half & Half','Cherry Grenadine']
+const phonedrinks = drinks.slice(0,7);
+if (window.matchMedia("(max-width: 600px)").matches) {
+    gameDrinks = phonedrinks;
+} else {
+    gameDrinks = drinks;
+}
