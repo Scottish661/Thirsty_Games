0339eb98862f7f3dc95a25198242f76e592897db
Scottish661 <h0785014@gmail.com>
2026-08-28 15:51:29 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello website"
+    message = "Hello website"
+    return render_template("index.html", message=message)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
