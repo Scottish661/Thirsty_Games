1ece0793648ccc1a68fc9f5650df01ce58b83ca5
Scottish661 <h0785014@gmail.com>
2026-08-28 12:04:33 +0100

    Update Logic.js

--- Logic.js
+++ Logic.js
@@ -21,18 +21,6 @@ let B = Math.floor(Math.random() * 256);
     button.innerHTML +=`<button style="background-color:rgb(${R},${G},${B});" 
     onclick="play('${drink}')"> ${drink} </button>`}
 drinks.forEach(make)
-for(let i = drinks.length-1; i > 0; i--){
-  let random = Math.floor(Math.random() * (i + 1));
- [drinks[i],drinks[random]] = [drinks[random],drinks[i]];
-}
-const rules = {}
-let wins = (drinks.length - 1) / 2
-for (let i = 0; i < drinks.length; i++) {
-  rules[drinks[i]] = []
-for(let j = 1; j <= wins; j++){
- let calculate  =  (i + j) % drinks.length
-rules[drinks[i]].push(drinks[calculate])
-}}
 function play(move){
   const cm = drinks[Math.floor(Math.random() * drinks.length)]
   let r = ""
