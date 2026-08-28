c9e91f99c02b0110ebd9d4f673fe1052c932cc75
Scottish661 <h0785014@gmail.com>
2026-08-28 12:08:22 +0100

    Create play.js

---
+++ play.js
@@ -0,0 +1,19 @@
+function play(move){
+  const cm = drinks[Math.floor(Math.random() * drinks.length)]
+  let r = ""
+  if (move === cm){
+    r = "tie"
+    score.Ties++
+  }
+  else if (rules[move].includes(cm)){
+    r = "You Win"
+    score.Wins++
+  }
+  else{
+    r = "You Lose"
+    score.Losses++
+  }
+  localStorage.setItem('score', JSON.stringify(score))
+ h.textContent = `you chose ${move}, computer chose ${cm}, ${r}\n` +`Wins: 
+ ${score.Wins} | Losses: ${score.Losses} | Ties: ${score.Ties}`}
+  for (let drink in rules) { show_rules.innerHTML += `${drink} → ${rules[drink].join(", ")}<br>`;}
