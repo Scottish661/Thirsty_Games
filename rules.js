44fd8278b8e6539f46584cfe4be24e55062104ba
Scottish661 <h0785014@gmail.com>
2026-08-28 12:48:15 +0100

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,12 +1,11 @@
-for(let i = drinks.length-1; i > 0; i--){
+for (let i = gameDrinks.length - 1; i > 0; i--) {
   let random = Math.floor(Math.random() * (i + 1));
- [drinks[i],drinks[random]] = [drinks[random],drinks[i]];
-}
-const rules = {}
-let wins = (drinks.length - 1) / 2
-for (let i = 0; i < drinks.length; i++) {
-  rules[drinks[i]] = []
-for(let j = 1; j <= wins; j++){
- let calculate  =  (i + j) % drinks.length
-rules[drinks[i]].push(drinks[calculate])
-}}
+  [gameDrinks[i], gameDrinks[random]] =
+  [gameDrinks[random], gameDrinks[i]];}
+const rules = {};
+let wins = (gameDrinks.length - 1) / 2;
+for (let i = 0; i < gameDrinks.length; i++) {
+  rules[gameDrinks[i]] = [];
+  for (let j = 1; j <= wins; j++) {
+    let calculate = (i + j) % gameDrinks.length;
+    rules[gameDrinks[i]].push(gameDrinks[calculate])}};
