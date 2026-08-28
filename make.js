cc4a24608107a9c86770b452783eac5c9a4ba482
Scottish661 <h0785014@gmail.com>
2026-08-28 12:43:27 +0100

    Update make.js

--- make.js
+++ make.js
@@ -4,4 +4,4 @@ let G = Math.floor(Math.random() * 256);
 let B = Math.floor(Math.random() * 256);
     button.innerHTML +=`<button style="background-color:rgb(${R},${G},${B});" 
     onclick="play('${drink}')"> ${drink} </button>`}
-drinks.forEach(make)
+gameDrinks.forEach(make)
