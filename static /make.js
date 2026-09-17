db9675b9e414fa609b516f1e3e884ea37783e64e
Scottish661 <h0785014@gmail.com>
2026-09-17 18:05:25 +0100

    Delete static  directory

--- static /make.js
+++
@@ -1,7 +0,0 @@
-function make(drink){
-let R = Math.floor(Math.random() * 256);
-let G = Math.floor(Math.random() * 256);
-let B = Math.floor(Math.random() * 256);
-    button.innerHTML +=`<button style="background-color:rgb(${R},${G},${B});" 
-    onclick="play('${drink}')"> ${drink} </button>`}
-gameDrinks.forEach(make)
--- static /reset.js
+++
@@ -1,6 +0,0 @@
-function resetGame(){
-  score.Wins = 0
-  score.Losses = 0
-  score.Ties = 0
-  localStorage.setItem('score', JSON.stringify(score))
-  alert("Score reset!")}
